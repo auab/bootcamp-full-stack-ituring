5de797d8945c5925d6632ad94936d70adb9c3b7b
Pedro Abreu <pedro.aurelio.abreu@gmail.com>
2022-10-07 10:04:01 -0300

    adicionar pratica-1.js

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -0,0 +1,19 @@
+let name = "Pedro"
+let email = "pedro.aurelio.abreu@gmail.com"
+let occupation = "engenheiro"
+let age = 28
+let placeToKnow =  "Pantanal"
+
+//VARIAVEIS
+console.log(name)
+console.log(email)
+console.log(occupation)
+console.log(age)
+console.log(placeToKnow)
+
+//TIPO DE VARIAVEL
+console.log(typeof(name))
+console.log(typeof(email))
+console.log(typeof(occupation))
+console.log(typeof(age))
+console.log(typeof(placeToKnow))
