8e0f45721ce4c3e3d5d65a09f145b32ab9c2d89e
Pedro Abreu <pedro.aurelio.abreu@gmail.com>
2022-10-07 14:16:50 -0300

    adicionar atividade 5

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -1,18 +1,18 @@
-let name = "Pedro"
+let firstName = "Pedro"
 let email = "pedro.aurelio.abreu@gmail.com"
 let occupation = "engenheiro"
 let age = 28
 let placeToKnow =  "Pantanal"
 
 //VARIAVEIS
-console.log(name)
+console.log(firstName)
 console.log(email)
 console.log(occupation)
 console.log(age)
 console.log(placeToKnow)
 
 //TIPO DE VARIAVEL
-console.log(typeof(name))
+console.log(typeof(firstName))
 console.log(typeof(email))
 console.log(typeof(occupation))
 console.log(typeof(age))
---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
@@ -0,0 +1,8 @@
+let firstName = "Pedro"
+let email = "pedro.aurelio.abreu@gmail.com"
+let occupation = "engenheiro"
+let age = 28
+let placeToKnow =  "Pantanal"
+
+let message = "Olá, "+firstName+ "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para "+ placeToKnow +". Confirma pra gente alguns dados? A sua idade é "+ age +", você é "+ occupation + "e o seu email é "+ email +"? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
+console.log(message)
