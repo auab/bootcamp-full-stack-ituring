d9cece1086734b246503494872fd8e896735991f
Pedro Abreu <pedro.aurelio.abreu@gmail.com>
2022-10-07 13:28:19 -0300

    adicionar pratica 3

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,17 @@
+let firstName = "Pedro"
+let email = "pedro.aurelio.abreu@gmail.com"
+let occupation = "engenheiro"
+let age = 28
+let placeToKnow =  "Pantanal"
+
+let nameType = typeof(firstName)
+let emailType = typeof(email)
+let occupationType = typeof(occupation)
+let ageType = typeof(age)
+let placeToKnowType = typeof(placeToKnow)
+
+console.log((nameType)=="string")
+console.log((emailType)=="string")
+console.log((occupationType)=="string")
+console.log((ageType)=="string")
+console.log((placeToKnowType)=="string")
