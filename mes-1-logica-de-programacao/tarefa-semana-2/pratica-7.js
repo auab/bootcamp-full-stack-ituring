e51aaa88a99113c2744c4e6e608dd1babb317293
Pedro Abreu <pedro.aurelio.abreu@gmail.com>
2022-10-14 22:38:53 -0300

    adicionar: pratica-7.js

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-7.js
@@ -0,0 +1,9 @@
+function bomDiaPoliglota(idioma){
+    if (idioma ==='pt')
+        console.log('Olá, Mundo!');
+    if (idioma ==='en')
+        console.log('Hello world!');
+    if (idioma ==='ge')
+        console.log('Hallo Welt!');
+}
+bomDiaPoliglota('pt');
