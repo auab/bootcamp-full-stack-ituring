4851776c82412ddf548c7439fad2394e96e29b85
Pedro Abreu <pedro.aurelio.abreu@gmail.com>
2022-10-16 23:33:09 -0300

    adicionar: pratica-10.js

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-10.js
@@ -0,0 +1,12 @@
+function maximo3(a,b,c){
+    if(a>b && a>c)
+        console.log(a);
+    else if(b>a && b>c)
+        console.log(b);
+    else    
+        console.log(c);
+}
+
+maximo3(1,2,3);
+maximo3(20,30,10);
+maximo3(300,200,100);
