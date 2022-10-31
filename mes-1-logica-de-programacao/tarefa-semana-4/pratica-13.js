ed2a893730cd24cc80c3857d05b34f6a5b658313
pedro.abreu <pedro.aurelio.abreu@gmail.com>
2022-10-31 09:02:13 -0300

    adidionar: pratica-13.js

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-13.js
@@ -0,0 +1,48 @@
+/*
+Crie uma função no arquivo pratica-13.js que ordena 
+uma sequência de movimentações bancárias começando 
+com a data mais recente até a data de movimentação 
+mais antiga.
+*/
+
+let transacoes = [{
+    valor: 100.00,
+    movimentacao: "deposito",
+    dataMovimentacao: "2021-08-02T07:46:36.611Z"
+  },
+  {
+    valor: 20.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2021-09-17T09:46:36.611Z"
+  },
+  {
+    valor: 30.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-02-24T09:46:36.611Z"
+  },
+  {
+    valor: 10.00,
+    movimentacao: "deposito",
+    dataMovimentacao: "2022-03-02T09:46:36.611Z"
+  },
+  {
+    valor: 7.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-04-20T09:46:36.611Z"
+  },
+  {
+    valor: 21.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-03-20T09:46:36.611Z"
+  },
+  {
+    valor: 5.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-06-08T09:46:36.611Z"
+  }];
+
+function ordenaTransacoes(transacoes){
+    return transacoes.sort((a,b)=>(Date.parse(b.dataMovimentacao)-Date.parse(a.dataMovimentacao)));
+}
+
+console.log(ordenaTransacoes(transacoes));
